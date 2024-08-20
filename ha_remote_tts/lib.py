1f49d782976f700a4563009d6375aa6f5ded8a89
NicolasNewman <nnewman316@gmail.com>
2024-08-19 19:19:56 -0500

    enabled running web server

--- ha_remote_tts/lib.py
+++ ha_remote_tts/lib.py
@@ -1,7 +1,9 @@
 """Module for provisioning a RemoteTTS Client/Server"""
 
 from aiohttp import ClientSession, web
+from asyncio import sleep
 from typing import Callable, Awaitable, Tuple
+import base64
 
 
 class RemoteTTSClient:
@@ -12,25 +14,25 @@ class RemoteTTSClient:
 		:param host: API url
 		"""
 		self._host = host
-		self._session = ClientSession(host)
 
-	async def synthesize(self, text: str) -> str:
+	async def synthesize(self, text: str) -> tuple[str, bytes]:
 		"""Synthesize the inputted text into audio by calling the API located at host
 
 		:param text: text to turn into spoken audio
 		"""
-		headers = {}
+		async with ClientSession(self._host) as session:
+			async with session.post('/synthesize', data={'text': text}) as resp:
+				data: dict = await resp.json(content_type='application/json; charset=utf-8')
+				audio_b64 = data.get('audio')
+				format = data.get('format')
+				if audio_b64 is None or type(audio_b64) is not str:
+					pass
+				if format is None or type(format) is not str:
+					pass
 
-		async with self._session.post('/synthesize', json={'text': text}) as resp:
-			data: dict = await resp.json()
-			audio = data.get('audio')
-			format = data.get('format')
-			if audio is None or type(audio) is not bytes:
-				pass
-			if format is None or type(format) is not str:
-				pass
+				audio = base64.b64decode(audio_b64)
 
-			return format, audio
+				return format, audio
 
 
 # https://docs.aiohttp.org/en/stable/web_quickstart.html#file-uploads
@@ -46,20 +48,31 @@ class RemoteTTSServer:
 		self._app = web.Application()
 		self._app.add_routes([web.post('/synthesize', self.synthesize)])
 		self._callback = callback
+		self._runner = web.AppRunner(self._app)
 
 	async def synthesize(self, request: web.Request) -> Awaitable[web.StreamResponse]:
 		"""Synthesization POST route
 
 		:param request: aiohttp request
 		"""
-
 		data = await request.post()
 		text = data.get('text')
 		if text is None:
-			# TODO handle errors
-			raise web.HTTPBadRequest(body="POST parameter 'text' is not defined")
+			return web.HTTPBadRequest(body="POST parameter 'text' is not defined")
 		if type(text) is not str:
-			raise web.HTTPUnsupportedMediaType(body="POST parameter 'text' is not a string")
+			return web.HTTPUnsupportedMediaType(body="POST parameter 'text' is not a string")
 
 		audio, format = self._callback(text)
-		return web.json_response({'format': format, 'audio': audio})
+		audio_str = base64.b64encode(audio)
+		return web.json_response({'format': format, 'audio': audio_str.decode('ascii')})
+
+	async def start(self, ip: str = 'localhost', port: str = '8080'):
+		await self._runner.setup()
+		site = web.TCPSite(self._runner, ip, port)
+		await site.start()
+
+		while True:
+			await sleep(3600)
+
+	async def stop(self):
+		await self._runner.cleanup()
